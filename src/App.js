4d21ffd225e7cea41a250b50b450b301fc5ae77f
Akobir <akobirbahronov07@gmail.com>
2021-10-25 01:50:17 +0500

    added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import { useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cardIsShown, setCardIsShown] = useState(false);
+
+  const showCardHandler = () => {
+    setCardIsShown(true);
+  };
+
+  const hideCardHandler = () => {
+    setCardIsShown(false);
+  };
   return (
     <CartProvider>
-      <Header />
+      {cardIsShown && <div>Cart...</div>}
+      <Header onShowCard={showCardHandler} />
       <main>
         <Meals />
       </main>
